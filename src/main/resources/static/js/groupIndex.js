d92f959c7a514625d69bb3d1bd8e0be400600e19
bvenk <bvenkataprudhvi@gmail.com>
2024-01-30 18:27:28 -0600

    A Working Functionality - Final Version 1.0: complete

--- src/main/resources/static/js/groupIndex.js
+++ src/main/resources/static/js/groupIndex.js
@@ -172,3 +172,22 @@ function postComment(text) {
             document.getElementById('sellSuggestionTable').style.display = 'block';
         }
     }
+function searchSuggestions(tableId, searchInputId) {
+            var input, filter, table, tr, td, i, txtValue;
+            input = document.getElementById(searchInputId);
+            filter = input.value.toUpperCase();
+            table = document.getElementById(tableId);
+            tr = table.getElementsByTagName("tr");
+
+            for (i = 0; i < tr.length; i++) {
+                td = tr[i].getElementsByTagName("td")[0]; // Change index based on the column you want to search
+                if (td) {
+                    txtValue = td.textContent || td.innerText;
+                    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                        tr[i].style.display = "";
+                    } else {
+                        tr[i].style.display = "none";
+                    }
+                }
+            }
+        }
